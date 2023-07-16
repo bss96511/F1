aeb5f321c8d35fbacaa5488be18e69e7dcca36c9
bss96511 <139694668+bss96511@users.noreply.github.com>
2023-07-16 21:48:42 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#macos.sh MAC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN MAC_REALNAME
+
+#disable spotlight indexing
+sudo mdutil -i off -a
+
+#Create new account
+sudo dscl . -create /Users/Gurpreet
+sudo dscl . -create /Users/Gurpreet UserShell /bin/bash
+sudo dscl . -create /Users/Gurpreet RealName $4
+sudo dscl . -create /Users/Gurpreet UniqueID 1001
+sudo dscl . -create /Users/Gurpreet PrimaryGroupID 80
+sudo dscl . -create /Users/Gurpreet NFSHomeDirectory /Users/tcv
+sudo dscl . -passwd /Users/Gurpreet "$MAC_USER_PASSWORD"
+sudo dscl . -passwd /Users/Gurpreet "$MAC_USER_PASSWORD"
+sudo createhomedir -c -u Gurpreet > /dev/null
+sudo dscl . -append /Groups/admin GroupMembership username
+
+#Enable VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes
+
+echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+
+#Start VNC/reset changes
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#install ngrok
+brew install ngrok/ngrok/ngrok
+
+#configure ngrok and start it
+ngrok authtoken 2Senfqvnaz8A3gPDfw1v9h2V7VT_74uqRQ5qsgYCtP9ReWYqV
+ngrok tcp 5900 --region=in &
